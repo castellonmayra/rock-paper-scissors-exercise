2290957c6bf0c00aa4a3f65a2fdb2150dca0bb34
castellonmayra <castellonmayra@gmail.com>
2022-07-13 06:22:10 -0400

    basic rock paper scissors code that worked

--- game.py
+++ game.py
@@ -1,3 +1,27 @@
 # this is the "game.py" file...
 
 print("Rock, Paper, Scissors, Shoot!")
+
+import random
+
+user = input("Enter a choice (rock, paper, scissors): ")
+possible_choices = ["rock", "paper", "scissors"]
+computer = random.choice(possible_choices)
+
+if user == computer:
+    print("It's a tie!")
+elif user == "rock":
+    if computer == "scissors":
+        print("Rock crushes scissors. You win!")
+    else:
+        print("Paper covers rock. You lose.")
+elif user == "paper":
+    if computer == "rock":
+        print("Paper covers rock. You win!")
+    else:
+        print("Scissors cuts paper. You lose.")
+elif user == "scissors":
+    if computer == "paper":
+        print("Scissors cuts paper. You win!")
+    else:
+        print("Rock crushes scissors. You lose.")
